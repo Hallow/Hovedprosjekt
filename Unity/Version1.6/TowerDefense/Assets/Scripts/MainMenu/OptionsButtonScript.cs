cccbbee6654eb6022d965a1e8d54a88539a5c89c
Aleksander Hougen <aleksander-hougen@hotmail.com>
2014-03-13 11:07:34 +0100

    Version 1.6
    - Working animations (all directions)
    - Game Loop is now 100%.
    - Static and customizable AI behaviour ready.
    - Some code for unit attacks

---
+++ Unity/Version1.6/TowerDefense/Assets/Scripts/MainMenu/OptionsButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel("nogo_scene");
+    }
+}
