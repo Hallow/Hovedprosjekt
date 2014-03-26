3a40e51e7344b4ee6c81094297f4b0d6d6fd2d72
geirmash <haugei11@nith.no>
2014-03-26 13:52:20 -0700

    New version
    Added "swipe" functionality.
    Added unit list (ongui).
    Added recruit button for recruiting units.

---
+++ Unity/Version1.6.2/TowerDefense/Assets/Scripts/PlayerReady.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerReady : MonoBehaviour {
+
+    //Ready is used to check whether the player has pressed the "End turn" button
+    public bool ready;
+
+	// Use this for initialization
+	void Start () {
+
+        ready = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        //The player has clicked "End Turn", so he is ready.
+        ready = true; 
+    }
+}
---
+++ Unity/Version1.6.2/TowerDefense/Assets/Scripts/Units/HeavyVikingScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeavyVikingScript : MonoBehaviour {
+
+	public int Health { get; set; }
+	public int Curse { get; set; }
+	public float Speed { get; set; }
+	public float Damage { get; set; }
+	
+	
+	// Use this for initialization
+	void Start () {
+		this.Health = 1;
+		this.Damage = 11.3f;
+		this.Speed = 5.0f; // Is not used at the moment. Speed of unit is set in WayPointScript.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Check curses.
+		
+		// Attack radius.
+		
+	}
+
+	public int GetHealth() {
+		return Health;
+	}
+	
+	public float GetSpeed() {
+		return Speed;
+	}
+
+	public void Attack() {
+		
+	}
+
+	public void Hurt(float damage) {
+		damage -= damage / 2.0f;
+		this.Health -= (int) damage;
+	}
+}
