0b500ee4589d30430f95ac88f0af5e4b636b156a
Aleksander Hougen <aleksander-hougen@hotmail.com>
2014-04-02 10:50:09 +0200

    Version 1.8.1
    Bugfixes

---
+++ Unity/Version1.8.1/TowerDefense/Assets/Scripts/GUI/RecruitButtonScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RecruitButtonScript : MonoBehaviour {
+    
+    public bool OpenMenu = false;
+
+    public GameObject viking1;
+    public GameObject viking2;
+
+    private GameObject viking_1;
+    private GameObject viking_2;
+
+    private bool ButtonsVisible = false;
+
+    public GameObject recruitmentController;
+
+	// Use this for initialization
+	void Start () {
+        viking1 = (GameObject)Resources.Load("RecruitBasicUnit");
+        viking2 = (GameObject)Resources.Load("RecruitHeavyUnit");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator OnMouseDown()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        if (!ButtonsVisible)
+        {
+            viking_1 = (GameObject)Instantiate(viking1);
+            viking_1.GetComponent<RecruitBasicScript>().recruitmentController = recruitmentController;
+
+            if (recruitmentController.GetComponent<RecruitmentScript>().loop.GetComponent<GameLoop>().player1.GetComponent<PlayerScript>().hasBarracks)
+            {
+                viking_2 = (GameObject)Instantiate(viking2);
+                viking_2.GetComponent<RecruitHeavyScript>().recruitmentController = recruitmentController;
+            }
+            
+
+            ButtonsVisible = true;
+        }
+        else
+        {
+            Destroy(viking_1);
+            Destroy(viking_2);
+
+            ButtonsVisible = false;
+        }
+    }
+}
