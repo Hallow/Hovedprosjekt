22033abf5f4db647dee7e71e2e97f8d8af2f86c5
geirmash <haugei11@nith.no>
2014-04-10 21:50:17 -0700

    fixed a bug when transitioning between the terrain_scene and woodland_scene

---
+++ Unity/Version1.8.7/TowerDefense/Assets/Scripts/RecruitButtons/RecruitBasicScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecruitBasicScript : MonoBehaviour {
+
+    public int index;
+    public GameObject recruitmentController;
+
+	// Use this for initialization
+	void Start () {
+
+        index = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("MOUSE DOWN!");
+        //index++;
+        recruitmentController.GetComponent<RecruitmentScript>().recruitmentBacklog.Add(0);
+    }
+}
