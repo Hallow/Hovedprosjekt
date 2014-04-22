0bc8da2c8885e7ce15ca7fd6db709887f8390609
dyrjon <dyrjon10@nith.no>
2014-04-22 12:35:28 +0200

    waypoints paa tutlevel, 1 og 2 forest (mappa heter version_Jonas)

---
+++ Unity/Version_Jonas/TowerDefense/Assets/Scripts/AiScript.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AiScript : MonoBehaviour {
+
+    int id;
+    int cash;
+    int income;
+
+    public GameObject unitFactory;
+    public GameObject factoryPrefab; //SET IN INSPECTOR
+
+    public List<GameObject> unitList;
+    public List<GameObject> towerList;
+
+    public GameObject aiStart;
+
+    public GameObject towerFactoryPrefab;
+    public GameObject towerFactory;
+
+	// Use this for initialization
+	void Start () {
+        cash = 150;
+        income = 150;
+        id = 1;
+
+
+        towerFactory = (GameObject)Instantiate(towerFactoryPrefab);
+        unitFactory = (GameObject)Instantiate(factoryPrefab);
+
+        unitList = new List<GameObject>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Removes units if theyre destroyed
+        foreach(GameObject u in unitList)
+        {
+            if(!u)
+            {
+                unitList.Remove(u);
+            }
+        }
+	
+	}
+
+    //Each CoRoutine below represents 1 round of AI behaviour. These coRoutines are called from the Game Loop.
+    public IEnumerator round1()
+    {
+
+        Vector3 slotCo = towerList[0].GetComponent<TowerScript>().transform.position;
+        //slotCo.y += 0.15f;
+        //slotCo.z -= 0.2f;
+        towerList.Add(towerFactory.GetComponent<TowerFactory>().spawnTower(slotCo, towerList[0].GetComponent<TowerScript>().towerType, towerList[0].GetComponent<TowerScript>().towerLevel + 1, towerList[0], false, 1));
+        towerList.Add(towerFactory.GetComponent<TowerFactory>().spawnTower(towerList[1].transform.position, towerList[1].GetComponent<TowerScript>().towerType, towerList[1].GetComponent<TowerScript>().towerLevel + 1, towerList[1], false, 1));
+
+
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+		Debug.Log("hei");
+        StopCoroutine("round1");
+    }
+
+    public IEnumerator round2()
+    {
+        towerList.Add(towerFactory.GetComponent<TowerFactory>().spawnTower(towerList[3].transform.position, towerList[3].GetComponent<TowerScript>().towerType, towerList[3].GetComponent<TowerScript>().towerLevel + 1, towerList[3], false, 1));
+        towerList.Add(towerFactory.GetComponent<TowerFactory>().spawnTower(towerList[1].transform.position, towerList[1].GetComponent<TowerScript>().towerType, towerList[1].GetComponent<TowerScript>().towerLevel + 1, towerList[1], false, 1));
+
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(2, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(2, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+        unitList.Add(unitFactory.GetComponent<UnitFactory>().spawnUnit(0, id, aiStart));
+        yield return new WaitForSeconds(1);
+
+        StopCoroutine("round2");
+    }
+}
---
+++ Unity/Version_Jonas/TowerDefense/Assets/Scripts/TerrainSelect/AsgardLevels/WoodlandLevelButtonScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoodlandLevelButtonScript : MonoBehaviour {
+
+    public int level;
+
+    public Texture2D clickedTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Click();
+        //Thread.Sleep(500);
+        //Application.LoadLevel("game_scene");
+
+        switch (level)
+        {
+            case 1:
+                Application.LoadLevel("game_scene");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+        }
+    }
+
+    public void Click()
+    {
+        //gameObject.renderer.material.mainTexture = clickedTexture;
+    }
+}
