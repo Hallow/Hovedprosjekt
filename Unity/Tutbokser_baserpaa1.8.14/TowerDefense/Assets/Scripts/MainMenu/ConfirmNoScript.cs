0b8bd42a69554580f48fc6e101d71f0e1246af42
dyrjon <dyrjon10@nith.no>
2014-05-15 15:56:18 +0200

    laget tut script om implementert

---
+++ Unity/Tutbokser_baserpaa1.8.14/TowerDefense/Assets/Scripts/MainMenu/ConfirmNoScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfirmNoScript : MonoBehaviour {
+
+    public GameObject interaction;
+
+	// Use this for initialization
+	void Start () {
+        interaction = (GameObject)Resources.Load("QuitConfirmMenuPrefab");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Camera.main.GetComponent<MainMenuInteractionScript1>().Close();
+        }
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Camera.main.GetComponent<MainMenuInteractionScript1>().Close();
+        }
+    }
+}
---
+++ Unity/Tutbokser_baserpaa1.8.14/TowerDefense/Assets/Scripts/SwipeInputScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class SwipeInputScript : MonoBehaviour
+{
+    public float speed;
+    public Vector2 touchStart;
+    public Vector2 touchEnd;
+
+    private float lastPosTime;
+    private float journeyLength;
+
+    private Vector3 destination;
+
+    int posCounter;
+
+    private Vector3 camPos1;
+    private Vector3 camPos2;
+
+    private bool InputHasEnded;
+
+    void Start()
+    {
+        Thread.Sleep(1000);
+        InputHasEnded = false;
+        camPos1 = Camera.main.transform.position;
+        //destination = new Vector3(0, Camera.main.transform.position.y + 500, 0.0f);
+        speed = 0.1f;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            if (Camera.main.transform.position.y != -19.61339f || Camera.main.transform.position.y != 1.011715f)
+                transform.Translate(0.0f, -touchDeltaPosition.y * speed, 0);
+
+            if (Camera.main.transform.position.y <= 1.011715f)
+            {
+                transform.position = new Vector2(0, 1.011714f);
+
+            }
+            else if (Camera.main.transform.position.y >= -19.61339f)
+            {
+                transform.position = new Vector2(0, -19.6134f);
+            }
+        }
+    }
+
+}
